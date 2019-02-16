90bd47cd9fd47c21882c556dda5ce533193da601
manticarodrigo <manticarodrigo@gmail.com>
2019-02-16 09:40:18 -0500

    Updated local host env variable and wp-config.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,4 +1,5 @@
 <?php
+define('WP_CACHE', true); // Added by WP Rocket
 /**
  * The base configuration for WordPress
  *
@@ -98,14 +99,14 @@ if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
 
 
 
-define( 'WP_ALLOW_MULTISITE', true );
-define( 'MULTISITE', true );
-define( 'SUBDOMAIN_INSTALL', true );
+define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
 $base = '/';
-define( 'DOMAIN_CURRENT_SITE', 'presshub.me' );
-define( 'PATH_CURRENT_SITE', '/' );
-define( 'SITE_ID_CURRENT_SITE', 1 );
-define( 'BLOG_ID_CURRENT_SITE', 1 );
+define('DOMAIN_CURRENT_SITE', getenv('NGINX_SERVER_NAME'));
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /* That's all, stop editing! Happy blogging. */
 
